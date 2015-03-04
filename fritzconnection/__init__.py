7c7c3fe62019d6af1ada69a985aa5a8284645760
Fettlaus <Fettlaus@gmail.com>
2015-03-04 13:16:49 +0000

    fixes #3

---
+++ fritzconnection/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from .fritzconnection import (
+    FritzConnection,
+    FritzInspection,
+    print_servicenames,
+    print_api,
+)
+from .fritzhosts import (
+    FritzHosts,
+    print_hosts,
+)
+from .fritzstatus import (
+    FritzStatus,
+    print_status,
+)
